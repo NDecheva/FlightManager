6518a9bcfc9d341526408db213227ca0390bfeb9
Nazdravec Pluvecski <hristofire@gmail.com>
2024-11-21 19:35:31 +0200

    Resolved issues in entities. Begin creation of DbContext

---
+++ FlightManager/FlightManager.Data/Entities/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManager.Data.Entities
+{
+    public class Booking : BaseEntity
+    {
+
+        public int FlightId { get; set; }
+        public int PersonalId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Nationality { get; set; }
+        public string SeatClass { get; set; }
+        public virtual List<Flight> Flights { get; set; }
+
+    }
+}
---
+++ FlightManager/FlightManager.Data/FlightManagerDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FlightManager.Data.Entities;
+
+namespace FlightManager.Data
+{
+    public class FlightManagerDbContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public FlightManagerDbContext(DbContextOptions<FlightManagerDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.Flight)
+                .WithMany(f => f.Bookings)
+                .HasForeignKey(b => b.FlightId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<Booking>()
+                .HasOne(b => b.User)
+                .WithMany(u => u.Bookings)
+                .HasForeignKey(b => b.PersonalId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<Flight>()
+                .HasMany(f => f.Bookings)
+                .WithOne(b => b.Flight)
+                .HasForeignKey(b => b.FlightId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Bookings)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.PersonalId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            
+            modelBuilder.Entity<User>()
+                .HasData(new User
+                {
+                    Id = 1,
+                    UserName = "admin",
+                    FirstName = "Admin",
+                    LastName = "User",
+                    Email = "admin@example.com",
+                    PhoneNumber = "1234567890",
+                    Password = "admin123", 
+                    PersonalId = 1,
+                    Address = "123 Admin St.",
+                    Role = "Admin"
+                });
+        }
+    }
+}
