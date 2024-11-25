023b6173e5285fb109e4d8f08843e2504c9d4819
Siyana05 <siyanaslavova95@gmail.com>
2024-11-25 19:12:03 +0200

    Added Dependency Injection

---
+++ FlightManager/FlightManager.Shared/Attributes/AutoBindAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManager.Shared.Attributes
+{
+    internal class AutoBindAttributes
+    {
+    }
+}
