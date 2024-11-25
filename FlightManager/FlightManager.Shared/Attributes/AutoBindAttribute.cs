26f092d2b20c0d9845ac97e2d4471fbece6a0b87
Siyana05 <siyanaslavova95@gmail.com>
2024-11-25 19:31:16 +0200

    Added Dependency Injection

--- FlightManager/FlightManager.Shared/Attributes/AutoBindAttribute.cs
+++ FlightManager/FlightManager.Shared/Attributes/AutoBindAttribute.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightManager.Shared.Attributes
 {
-    internal class AutoBindAttributes
+    public class AutoBindAttribute : Attribute
     {
     }
 }
--- FlightManager/FlightManager.Shared/Extensions/ServiceCollectionExtensions.cs
+++ FlightManager/FlightManager.Shared/Extensions/ServiceCollectionExtensions.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using FlightManager.Shared.Attributes;
+using FlightManager.Shared.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlightManager.Shared.Extensions
 {
-    internal class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static void AutoBind(this IServiceCollection source, params Assembly[] assemblies)
+        {
+            source.Scan(scan => scan.FromAssemblies(assemblies)
+            .AddClasses(classes => classes.WithAttribute<AutoBindAttribute>())
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+        }
     }
 }
