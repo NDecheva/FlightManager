d2204c06e3fc3ac924190e164b35a10fd5d0854a
Nazdravec <hristofire@gmail.com>
2024-11-30 15:36:30 +0200

    Added Repositories and IBaseEntitiy

---
+++ FlightManager/FlightManager.Data/Repos/BookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FlightManager.Data.Entities;
+using FlightManager.Shared.Attributes;
+using FlightManager.Shared.Dtos;
+
+namespace FlightManager.Data.Repos
+{
+    [AutoBind]
+    // TO DO:
+    public class BookingRepository : BaseRepository<Booking, BookingDto>, IBookingRepository
+    {
+        public BookingRepository(FlightManagerDbContext context, IMapper mapper) : base(context, mapper)
+        {
+
+        }
+    }
+}
