d606f9c1604dd006db00e4de04a97417c18bcd8a
Nazdravec <hristofire@gmail.com>
2024-11-30 17:55:55 +0200

    Added Repositories and Repository Tests

---
+++ FlightManager/FlightManager.Tests/Repos/FlightRepositoryTests.cs
@@ -0,0 +1,11 @@
+﻿using FlightManager.Data.Entities;
+using FlightManager.Data.Repos;
+using FlightManager.Shared.Dtos;
+
+namespace FlightManager.Tests.Repos
+{
+    public class FlightRepositoryTests :
+         BaseRepositoryTests<FlightRepository, Flight, FlightDto>
+    {
+    }
+}
