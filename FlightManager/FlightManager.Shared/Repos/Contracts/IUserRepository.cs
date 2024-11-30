bf8f2ceff195402e48ed363237970dacb7c7a6a0
Siyana05 <siyanaslavova95@gmail.com>
2024-11-30 14:44:50 +0200

    Create Interfaces

---
+++ FlightManager/FlightManager.Shared/Repos/Contracts/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using FlightManager.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManager.Shared.Repos.Contracts
+{
+    public interface IUserRepository : IBaseRepository<UserDto>
+    {
+
+    }
+
+}
