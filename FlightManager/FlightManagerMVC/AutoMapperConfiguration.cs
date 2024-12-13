32d0580774d3aa6aafe19b1dd9f3245bed85aa28
siyanaslavova95@gmail.com <>
2024-12-13 11:08:29 +0200

    Added AutoMapper but not all code

---
+++ FlightManager/FlightManagerMVC/AutoMapperConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FlightManager.Data.Entities;
+using FlightManager.Shared.Dtos;
+
+namespace FlightManagerMVC
+{
+    internal class AutoMapperConfiguration : Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<Booking, BookingDto>().ReverseMap();
+            
+
+            CreateMap<Flight, FlightDto>().ReverseMap();
+            
+
+            CreateMap<User, UserDto>().ReverseMap();
+           
+
+        }
+    }
+}
