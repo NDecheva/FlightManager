43dfb31e62033b36a020443107c550d77f128cf1
Nazdravec <hristofire@gmail.com>
2024-12-19 23:43:29 +0200

    Added Crud Controllers

---
+++ FlightManager/FlightManagerMVC/Controllers/BookingController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+
+using FlightManager.Shared.Dtos;
+using FlightManager.Shared.Repos.Contracts;
+using FlightManager.Shared.Services.Contracts;
+using FlightManagerMVC.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FlightManagerMVC.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Admin, Employee, User")]
+    public class BookingController : BaseCrudController<BookingDto, IBookingRepository, IBookingsService, BookingEditVM ,BookingDetailsVM>
+
+    {
+       
+        public BookingController(IBookingsService service,IMapper mapper): base(service,mapper)
+        {
+
+        }
+       
+        
+                
+        
+
+    }
+}
---
+++ FlightManager/FlightManagerMVC/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FlightManager.Shared.Dtos;
+using FlightManager.Shared.Repos.Contracts;
+using FlightManager.Shared.Services.Contracts;
+using FlightManagerMVC.ViewModels;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FlightManagerMVC.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Admin, Employee, User")]
+    public class UserController : BaseCrudController<UserDto, IUserRepository, IUsersService, UserEditVM, UserDetailsVM>
+    {
+        public UserController(IUsersService service, IMapper mapper): base(service, mapper)
+        {
+                
+        }
+
+    }
+}
