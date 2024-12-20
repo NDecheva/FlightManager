e28d2b212c03043e3d5dc527ef8617d0780e515d
Siyana05 <siyanaslavova95@gmail.com>
2024-12-20 11:17:58 +0200

    Finish-AutoMapper

--- FlightManager/FlightManagerMVC/AutoMapperConfiguration.cs
+++ FlightManager/FlightManagerMVC/AutoMapperConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FlightManager.Data.Entities;
 using FlightManager.Shared.Dtos;
+using FlightManagerMVC.ViewModels;
 
 namespace FlightManagerMVC
 {
@@ -9,13 +10,19 @@ namespace FlightManagerMVC
         public AutoMapperConfiguration()
         {
             CreateMap<Booking, BookingDto>().ReverseMap();
-            
+            CreateMap<BookingDto, BookingEditVM>().ReverseMap();
+            CreateMap<BookingDto, BookingDetailsVM>().ReverseMap();
+
 
             CreateMap<Flight, FlightDto>().ReverseMap();
-            
+            CreateMap<FlightDto, FlightEditVM>().ReverseMap();
+            CreateMap<FlightDto, FlightDetailsVM>().ReverseMap();
+
 
             CreateMap<User, UserDto>().ReverseMap();
-           
+            CreateMap<UserDto, UserEditVM>().ReverseMap();
+            CreateMap<UserDto, UserDetailsVM>().ReverseMap();
+
 
         }
     }
