3234767cf7f542c1f524fcf3cc9a50882e7e4b29
Kitaeca <104841268+KitaecaCapalot@users.noreply.github.com>
2025-01-25 23:25:18 +0200

    Update Program.cs

--- FlightManager/FlightManagerMVC/Program.cs
+++ FlightManager/FlightManagerMVC/Program.cs
@@ -4,10 +4,6 @@ using FlightManager.Services;
 using FlightManagerMVC;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
-using FlightManager.Shared.Services.Contracts;
-using FlightManager.Shared.Repos.Contracts;  // Ensure this import is added
-using FlightManager.Services;  // Ensure this import is added for BookingsService
-using FlightManager.Data.Repos;
 using FlightManager.Shared.Extensions;
 using EntityFrameworkCore.UseRowNumberForPaging;
 
@@ -16,13 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAutoMapper(m => m.AddProfile(new AutoMapperConfiguration()));
-
-builder.Services.AutoBind(typeof(BookingsService).Assembly);
-builder.Services.AutoBind(typeof(BookingRepository).Assembly);
-
 builder.Services.AddDbContext<FlightManagerDbContext>(options =>
 {
+
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"],
         r => r.UseRowNumberForPaging());
 });
@@ -40,7 +32,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<FlightManagerDbContext>();
-    // Automatically update the database
+    // Automatically update database
     context.Database.Migrate();
 }
 
@@ -48,6 +40,7 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
