02f4a4ec5789e1482dd02dace68488df4ac949e6
Nazdravec <hristofire@gmail.com>
2025-02-10 12:31:01 +0200

    Veri gud n bad

--- FlightManager/FlightManagerMVC/Controllers/BookingController.cs
+++ FlightManager/FlightManagerMVC/Controllers/BookingController.cs
@@ -7,6 +7,7 @@ using FlightManagerMVC.Enums;
 using FlightManagerMVC.ViewModels;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FlightManagerMVC.Controllers
@@ -16,10 +17,13 @@ namespace FlightManagerMVC.Controllers
     public class BookingController : BaseCrudController<BookingDto, IBookingRepository, IBookingsService, BookingEditVM, BookingDetailsVM>
     {
         protected readonly IFlightsService _flightService;
-
-        public BookingController(IBookingsService service, IFlightsService flightService, IMapper mapper) : base(service, mapper)
+        protected readonly IBookingsService _bookingService;
+        protected readonly IUsersService _userService;
+        public BookingController(IBookingsService service, IFlightsService flightService,IUsersService usersService, IMapper mapper) : base(service, mapper)
         {
             this._flightService = flightService;
+            this._bookingService = service;
+            this._userService = usersService;
         }
 
         protected override async Task<BookingEditVM> PrePopulateVMAsync(BookingEditVM editVM)
@@ -31,6 +35,28 @@ namespace FlightManagerMVC.Controllers
 .Select(x => new SelectListItem($"{x.AircraftId} | {x.DepartureLocation} - {x.ArrivalLocation}", x.Id.ToString()));
 
             return editVM;
+        }
+        [HttpGet]
+        [Route("Booking/Search")]
+        public async Task<IActionResult> Search(string searchTerm)
+        {
+            ViewBag.SearchTerm = searchTerm;
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                return RedirectToAction(nameof(List));
+            }
+
+            var bookings = await _userService.GetAllAsync();
+
+            var filteredBookings = bookings.Where(u =>
+                u.Email.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            var bookingVMs = _mapper.Map<IEnumerable<UserDetailsVM>>(filteredBookings);
+
+            ViewBag.BookingVM = bookingVMs;
+
+            return View("List", bookingVMs);
         }
     }
 }
